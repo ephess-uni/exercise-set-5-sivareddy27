9ea1543dbba07d8c95541109c9034664db9b0f28
sivareddy27 <157670088+sivareddy27@users.noreply.github.com>
2024-02-29 14:28:44 -0600

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -20,7 +20,11 @@ if __name__ == "__main__":
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
+    data = np..loadtxt(INFILE)
+    data_mean = np.mean(data)
+    data -= data_mean
+    data_std = np.std(data)
+    data /= data_std
+    processed = data
+    np.savetxt(OUTFILE,processed)
 
-    # Complete the data processing steps using numpy here.
-
-    # Save the output to OUTFILE using numpy routines.
